fc0c105d7e1851161ea7678e8676bf85ae033d9e
Arciiix <artur.nowak100@gmail.com>
2021-12-25 14:36:06 +0100

    Add logging library

--- core/index.ts
+++ core/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { Server as SocketServer, Socket } from "socket.io";
 import dotenv from "dotenv";
+import logger from "./utils/logger";
+
+const { info, error, warn, debug } = logger;
 
 //Load environment variables from file
 dotenv.config();
-
 const PORT = process.env.PORT || 5555;
 
 const app = express();
 const server = app.listen(PORT, () => {
-  //TODO: Migrate to a logging library
-  console.log(`Core has started on port ${PORT}.`);
+  info(`Core has started on port ${PORT}.`);
 });
 
 const io = new SocketServer(server, {
@@ -20,8 +21,7 @@ const io = new SocketServer(server, {
 });
 
 io.on("connection", (socket: Socket) => {
-  //TODO: Migrate to a logging library
-  console.log(
+  info(
     `New socket with id ${
       socket.id
     } has connected! Request object: ${JSON.stringify(socket.handshake)}`
---
+++ core/utils/logger.ts
@@ -0,0 +1,35 @@
+import * as winston from "winston";
+import DailyRotateFile = require("winston-daily-rotate-file");
+
+const transport = new DailyRotateFile({
+  filename: "./logs/buzzine-core-%DATE%.log",
+  datePattern: "DD.MM.YYYY",
+  zippedArchive: true,
+  maxFiles: "14d",
+  level: "info",
+});
+
+const format = winston.format.printf((info) => {
+  return `[${info.level}] [${info.timestamp}]${
+    info.label ? " " + info.label : ""
+  }: ${info.message}`;
+});
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "DD.MM.YYYY HH:mm:ss" }),
+    format
+  ),
+  transports: [
+    transport,
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: "DD.MM.YYYY HH:mm:ss" }),
+        format
+      ),
+    }),
+  ],
+});
+
+export default logger;
