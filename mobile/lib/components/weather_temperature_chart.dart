23df180270dd646161bd0e5abb8b481dea73e19f
Arciiix <artur.nowak100@gmail.com>
2022-02-06 13:44:14 +0100

    Change the weather temperature chart type on mobile

--- mobile/lib/components/weather_temperature_chart.dart
+++ mobile/lib/components/weather_temperature_chart.dart
@@ -3,6 +3,7 @@ import 'package:buzzine/types/Weather.dart';
 import 'package:buzzine/utils/formatting.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'dart:math';
 
 class WeatherTemperatureChart extends StatelessWidget {
   const WeatherTemperatureChart({Key? key}) : super(key: key);
@@ -14,24 +15,34 @@ class WeatherTemperatureChart extends StatelessWidget {
       child: Container(
         height: 300,
         width: MediaQuery.of(context).size.width,
-        child: charts.BarChart(
+        child: charts.TimeSeriesChart(
           [
-            charts.Series<Weather, String>(
+            charts.Series<Weather, DateTime>(
               id: 'temperature',
               colorFn: (_, __) => charts.MaterialPalette.white,
-              domainFn: (Weather weather, _) =>
-                  addZero(weather.timestamp.toLocal().hour),
+              domainFn: (Weather weather, _) => weather.timestamp.toLocal(),
               measureFn: (Weather weather, _) => weather.temperature,
               data: GlobalData.weather!.hourly,
-            )
+            )..setAttribute(charts.rendererIdKey, "temperatureArea")
           ],
-          domainAxis: charts.OrdinalAxisSpec(
-              renderSpec: charts.SmallTickRendererSpec(
-                  labelStyle: charts.TextStyleSpec(
-                      color: charts.MaterialPalette.white))),
+          customSeriesRenderers: [
+            charts.LineRendererConfig(
+                customRendererId: 'temperatureArea',
+                includeArea: true,
+                stacked: true),
+          ],
+          domainAxis: charts.DateTimeAxisSpec(
+            renderSpec: charts.SmallTickRendererSpec(
+                labelStyle:
+                    charts.TextStyleSpec(color: charts.MaterialPalette.white)),
+            tickFormatterSpec: charts.BasicDateTimeTickFormatterSpec(
+                (DateTime? val) => addZero(val!.hour)),
+          ),
           animate: true,
           animationDuration: Duration(milliseconds: 500),
           primaryMeasureAxis: charts.NumericAxisSpec(
+              tickProviderSpec:
+                  charts.BasicNumericTickProviderSpec(zeroBound: false),
               tickFormatterSpec:
                   charts.BasicNumericTickFormatterSpec((num? val) => "$val°C"),
               renderSpec: charts.GridlineRendererSpec(
