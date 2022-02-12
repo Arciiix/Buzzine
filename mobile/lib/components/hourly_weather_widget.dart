430e91a1365e6b6c86aa0bc873f5ad89488ca21c
Arciiix <artur.nowak100@gmail.com>
2022-02-12 23:14:00 +0100

    Display detailed weather data on hourly weather

--- mobile/lib/components/hourly_weather_widget.dart
+++ mobile/lib/components/hourly_weather_widget.dart
@@ -1,41 +1,78 @@
+import 'package:buzzine/components/weather_widget.dart';
+import 'package:buzzine/globalData.dart';
 import 'package:buzzine/types/Weather.dart';
 import 'package:buzzine/utils/formatting.dart';
 import 'package:buzzine/utils/weather_icons_mapping.dart';
 import 'package:flutter/material.dart';
 
-class HourlyWeather extends StatelessWidget {
+class HourlyWeather extends StatefulWidget {
   final Weather weather;
   final bool? darkMode;
   const HourlyWeather({Key? key, required this.weather, this.darkMode})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-                bottom: 15), //The icons in the font aren't centered
-            child: Icon(
-              getIconData(weather.weatherIcon),
-              color: darkMode == true ? Colors.white : Colors.black,
-              size: 30,
-            ),
+  State<HourlyWeather> createState() => _HourlyWeatherState();
+}
+
+class _HourlyWeatherState extends State<HourlyWeather> {
+  void navigateToDetailedData() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+              "${GlobalData.weather!.cityName} - ${addZero(widget.weather.timestamp.toLocal().hour)}:${addZero(widget.weather.timestamp.toLocal().minute)} ${addZero(widget.weather.timestamp.toLocal().day)}.${addZero(widget.weather.timestamp.toLocal().month)}.${widget.weather.timestamp.toLocal().year}"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              WeatherWidgetDisplay(
+                weather: widget.weather,
+                backgroundColor: Theme.of(context).cardColor,
+              ),
+              DetailedWeatherData(weather: widget.weather)
+            ],
           ),
-          Text(
-              "${addZero(weather.timestamp.toLocal().hour)}:${addZero(weather.timestamp.toLocal().minute)}",
-              style: TextStyle(
-                fontSize: 20,
-                color: darkMode == true ? Colors.white : Colors.black,
-              )),
-          Text("${weather.temperature}°C",
-              style: TextStyle(
-                fontSize: 15,
-                color: darkMode == true ? Colors.white : Colors.black,
-              ))
-        ],
+          actions: <Widget>[
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("OK")),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: navigateToDetailedData,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 15), //The icons in the font aren't centered
+              child: Icon(
+                getIconData(widget.weather.weatherIcon),
+                color: widget.darkMode == true ? Colors.white : Colors.black,
+                size: 30,
+              ),
+            ),
+            Text(
+                "${addZero(widget.weather.timestamp.toLocal().hour)}:${addZero(widget.weather.timestamp.toLocal().minute)}",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: widget.darkMode == true ? Colors.white : Colors.black,
+                )),
+            Text("${widget.weather.temperature}°C",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: widget.darkMode == true ? Colors.white : Colors.black,
+                ))
+          ],
+        ),
       ),
     );
   }
--- mobile/lib/components/weather_widget.dart
+++ mobile/lib/components/weather_widget.dart
@@ -1,6 +1,8 @@
 import 'package:buzzine/components/hourly_weather_widget.dart';
 import 'package:buzzine/globalData.dart';
 import 'package:buzzine/screens/loading.dart';
+import 'package:buzzine/types/Weather.dart';
+import 'package:buzzine/utils/formatting.dart';
 import 'package:buzzine/utils/weather_icons_mapping.dart';
 import "package:flutter/material.dart";
 import 'package:flutter/rendering.dart';
@@ -40,75 +42,12 @@ class _WeatherWidgetState extends State<WeatherWidget> {
   Widget build(BuildContext context) {
     if (isLoaded) {
       return Material(
-        color: Colors.transparent,
-        child: Container(
-          decoration: BoxDecoration(
-            color: widget.backgroundColor ?? Colors.white,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          padding: EdgeInsets.all(8),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            bottom: 30), //The icons in the font aren't centered
-                        child: Icon(
-                          getIconData(GlobalData.weather!.current.weatherIcon),
-                          color: (widget.backgroundColor ?? Colors.white)
-                                      .computeLuminance() >
-                                  0.5
-                              ? Colors.black
-                              : Colors.white,
-                          size: 60,
-                        ),
-                      )),
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      children: [
-                        Text("${GlobalData.weather!.current.temperature}°C",
-                            style: TextStyle(
-                                color: (widget.backgroundColor ?? Colors.white)
-                                            .computeLuminance() >
-                                        0.5
-                                    ? Colors.black
-                                    : Colors.white,
-                                fontSize: 40)),
-                        Text(GlobalData.weather!.current.weatherDescription,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: (widget.backgroundColor ?? Colors.white)
-                                            .computeLuminance() >
-                                        0.5
-                                    ? Colors.black
-                                    : Colors.white,
-                                fontSize: 25)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                      children: GlobalData.weather!.hourly
-                          .map((e) => HourlyWeather(
-                              weather: e,
-                              darkMode: (widget.backgroundColor ?? Colors.white)
-                                      .computeLuminance() <
-                                  0.5))
-                          .toList()))
-            ],
-          ),
-        ),
-      );
+          color: Colors.transparent,
+          child: WeatherWidgetDisplay(
+            weather: GlobalData.weather!.current,
+            hourlyWeather: GlobalData.weather!.hourly,
+            backgroundColor: widget.backgroundColor,
+          ));
     } else {
       return Material(
         color: Colors.transparent,
@@ -138,5 +77,234 @@ class _WeatherWidgetState extends State<WeatherWidget> {
             ])),
       );
     }
+  }
+}
+
+class WeatherWidgetDisplay extends StatelessWidget {
+  final Color? backgroundColor;
+  final Weather weather;
+  final List<Weather>? hourlyWeather;
+  const WeatherWidgetDisplay(
+      {Key? key,
+      this.backgroundColor,
+      required this.weather,
+      this.hourlyWeather})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor ?? Colors.white,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: EdgeInsets.all(8),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        bottom: 30), //The icons in the font aren't centered
+                    child: Icon(
+                      getIconData(GlobalData.weather!.current.weatherIcon),
+                      color:
+                          (backgroundColor ?? Colors.white).computeLuminance() >
+                                  0.5
+                              ? Colors.black
+                              : Colors.white,
+                      size: 60,
+                    ),
+                  )),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text("${weather.temperature}°C",
+                        style: TextStyle(
+                            color: (backgroundColor ?? Colors.white)
+                                        .computeLuminance() >
+                                    0.5
+                                ? Colors.black
+                                : Colors.white,
+                            fontSize: 40)),
+                    Text(weather.weatherDescription,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: (backgroundColor ?? Colors.white)
+                                        .computeLuminance() >
+                                    0.5
+                                ? Colors.black
+                                : Colors.white,
+                            fontSize: 25)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: hourlyWeather != null
+                      ? hourlyWeather!
+                          .map((e) => HourlyWeather(
+                              weather: e,
+                              darkMode: (backgroundColor ?? Colors.white)
+                                      .computeLuminance() <
+                                  0.5))
+                          .toList()
+                      : []))
+        ],
+      ),
+    );
+  }
+}
+
+class DetailedWeatherData extends StatefulWidget {
+  final Weather weather;
+
+  const DetailedWeatherData({Key? key, required this.weather})
+      : super(key: key);
+
+  @override
+  _DetailedWeatherDataState createState() => _DetailedWeatherDataState();
+}
+
+class _DetailedWeatherDataState extends State<DetailedWeatherData> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      "Szybkość wiatru",
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "${widget.weather.windSpeed.toString()} m/s",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(height: 20, color: Colors.white, width: 2),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      "Ciśnienie",
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "${widget.weather.pressure.toString()} hPa",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      "Wilgotność",
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "${widget.weather.humidity.toString()}%",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(height: 20, color: Colors.white, width: 2),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      "Zachmurzenie",
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "${widget.weather.clouds.toString()}%",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: (widget.weather.sunrise != null &&
+                    widget.weather.sunset != null)
+                ? [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Godz. wschodu",
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "${addZero(widget.weather.sunrise!.toLocal().hour)}:${addZero(widget.weather.sunrise!.toLocal().minute)}",
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child:
+                          Container(height: 20, color: Colors.white, width: 2),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Godz. zachodu",
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "${addZero(widget.weather.sunset!.toLocal().hour)}:${addZero(widget.weather.sunset!.toLocal().minute)}",
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]
+                : [],
+          ),
+        ),
+      ],
+    );
   }
 }
