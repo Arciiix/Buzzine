761d055195a6c697f14d9f9689080b45e48e988d
Arciiix <artur.nowak100@gmail.com>
2022-02-24 18:51:29 +0100

    Add some logging to the database modules

--- audio/utils/db.ts
+++ audio/utils/db.ts
@@ -6,6 +6,19 @@ const db = new PrismaClient();
 async function initDatabase() {
   await db.$connect();
   logger.info(`Connected to the database`);
+
+  //Logging
+  db.$use(async (params, next) => {
+    const before = Date.now();
+    const result = await next(params);
+    const after = Date.now();
+
+    logger.debug(
+      `Query ${params.model}.${params.action} took ${after - before}ms`
+    );
+
+    return result;
+  });
 }
 
 export default db;
